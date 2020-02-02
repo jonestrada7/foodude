bfd53299855172f94687aab1991db8e41d93c074
jonestrada7 <jonathan.estrada747@gmail.com>
2020-02-02 01:38:17 -0800

    Implemented React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import SearchBar from './SearchBar.js';
 import Card from './Card.js';
@@ -26,22 +32,26 @@ class App extends React.Component {
   assignEntriesArray = (entriesArray) => {
     this.setState({entriesArray: entriesArray});
   }
+  
+  toggleSideBar = () =>{
+    document.getElementById("sidebar").classList.toggle('active');
+   }
 
-  assignMenuChoice = (menuChoice) => {
-    this.setState({menuChoice: menuChoice});
-  }
-
+  // assignMenuChoice = (menuChoice) => {
+  //   this.setState({menuChoice: menuChoice});
+  // }
+   
   renderPage = () => {
     if (this.state.menuChoice == 0 ){
-      return <React.Fragment>
-        <SearchBar assignEntriesArray ={this.assignEntriesArray}></SearchBar>
-        { this.state.entriesArray.map((foodEntry,index) => 
-          <Card name = {foodEntry.name} price = {foodEntry.price} 
-          description = {foodEntry.description} locations = {foodEntry.locations} image = {foodEntry.image} key={index}>
-          </Card>
-        )
-        }
-      </React.Fragment>
+      // return <React.Fragment>
+      //   <SearchBar assignEntriesArray ={this.assignEntriesArray}></SearchBar>
+      //   { this.state.entriesArray.map((foodEntry,index) => 
+      //     <Card name = {foodEntry.name} price = {foodEntry.price} 
+      //     description = {foodEntry.description} locations = {foodEntry.locations} image = {foodEntry.image} key={index}>
+      //     </Card>
+      //   )
+      //   }
+      // </React.Fragment>
     
     } else {
       return <React.Fragment>
@@ -52,8 +62,43 @@ class App extends React.Component {
 
     render(){
       return <React.Fragment>
-        <Header assignMenuChoice = {this.assignMenuChoice}></Header>
-        {this.renderPage()}
+        <Router>
+        <div id="sidebar">
+          <div className="toggle-btn" onClick={this.toggleSideBar}>
+            <a>
+              <span></span>
+              <span></span>
+              <span></span>
+            </a>
+              </div>
+                <ul id = "menuList">
+                  {/* onClick = {this.menuClick(this.id)} */}
+                  <li id = "0"  style={{"fontFamily": "\'Merriweather\', serif","backgroundColor": "#353b48","padding":"20px", "color":"white"}}><Link to="/"style={{"color":"beige","textDecoration": "none"}} >Home</Link></li>
+                  <li id = "1" style= {{"fontFamily": "\'Merriweather\', serif","backgroundColor": "#353b48","padding":"20px", "color":"white"}}><Link to="/create" style={{"color":"beige","textDecoration": "none"}} >Upload</Link></li>
+                  <li id = "2" style={{"fontFamily": "\'Merriweather\', serif", "backgroundColor": "#353b48","padding":"20px", "color":"white"}}><Link to="/about" style={{"color":"beige","textDecoration": "none"}} >About</Link></li>
+                </ul>
+              </div>
+              <center> 
+                <h1 style={{"fontFamily": "\'Merriweather\', serif","backgroundColor": "#353b48","padding":"20px", "color":"white"}}>
+                  FOODUDE 
+                  <img src="https://i.imgur.com/pkMF0BL.png" style={{"width":"9vh"}}></img>
+                </h1>
+                  </center>
+            <Switch>
+              <Route exact path="/">
+                  <SearchBar assignEntriesArray ={this.assignEntriesArray}></SearchBar>
+                  { this.state.entriesArray.map((foodEntry,index) => 
+                    <Card name = {foodEntry.name} price = {foodEntry.price} 
+                    description = {foodEntry.description} locations = {foodEntry.locations} image = {foodEntry.image} key={index}>
+                    </Card>
+                  )}
+              </Route>
+              <Route path="/create">
+                <Form></Form>
+              </Route>
+            </Switch>
+          {this.renderPage()}
+        </Router>
       </React.Fragment>
     }
 }
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,4 +1,13 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import App from "./App";
+import Form from "./Form";
 
 class Header extends React.Component{
 
@@ -9,38 +18,39 @@ class Header extends React.Component{
             
         }
     }
-    toggleSideBar = () =>{
-        document.getElementById("sidebar").classList.toggle('active');
-       }
+    // toggleSideBar = () =>{
+    //     document.getElementById("sidebar").classList.toggle('active');
+    //    }
     
-    menuClick = (position) => {
-        this.props.assignMenuChoice(position);   
-    }
+    // menuClick = (position) => {
+    //     this.props.assignMenuChoice(position);   
+    // }
 
-    render(){
-        return <React.Fragment>
-            <div id="sidebar">
-                <div className="toggle-btn" onClick={this.toggleSideBar}>
-                <a>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </a>
-                </div>
-                <ul id = "menuList">
-                    <li id = "0" onClick = {this.menuClick(this.id)} style={{"fontFamily": "\'Merriweather\', serif","backgroundColor": "#353b48","padding":"20px", "color":"white"}}><a style={{"color":"beige","textDecoration": "none"}} href="foodude.html">Home</a></li>
-                    <li id = "1" onClick = {this.menuClick(this.id)} style= {{"fontFamily": "\'Merriweather\', serif","backgroundColor": "#353b48","padding":"20px", "color":"white"}}><a  style={{"color":"beige","textDecoration": "none"}}href="foodude2.html">Upload</a></li>
-                    <li id = "2" onClick = {this.menuClick(this.id)} style={{"fontFamily": "\'Merriweather\', serif", "backgroundColor": "#353b48","padding":"20px", "color":"white"}}><a  style={{"color":"beige","textDecoration": "none"}}href="foodude3.html">About</a></li>
-                </ul>
-            </div>
-                <center> 
-                    <h1 style={{"fontFamily": "\'Merriweather\', serif","backgroundColor": "#353b48","padding":"20px", "color":"white"}}>
-                        FOODUDE 
-                        <img src="https://i.imgur.com/pkMF0BL.png" style={{"width":"9vh"}}></img>
-                    </h1>
-                </center>
-        </React.Fragment>
-    }
+    // render(){
+    //     return <React.Fragment>
+    //         <div id="sidebar">
+    //             <div className="toggle-btn" onClick={this.toggleSideBar}>
+    //             <a>
+    //                 <span></span>
+    //                 <span></span>
+    //                 <span></span>
+    //             </a>
+    //             </div>
+    //             <ul id = "menuList">
+    //               {/* onClick = {this.menuClick(this.id)} */}
+    //                 <li id = "0"  style={{"fontFamily": "\'Merriweather\', serif","backgroundColor": "#353b48","padding":"20px", "color":"white"}}><Link to="/" style={{"color":"beige","textDecoration": "none"}} >Home</Link></li>
+    //                 <li id = "1" style= {{"fontFamily": "\'Merriweather\', serif","backgroundColor": "#353b48","padding":"20px", "color":"white"}}><Link to="/create" style={{"color":"beige","textDecoration": "none"}} >Upload</Link></li>
+    //                 <li id = "2" style={{"fontFamily": "\'Merriweather\', serif", "backgroundColor": "#353b48","padding":"20px", "color":"white"}}><Link to="/about" style={{"color":"beige","textDecoration": "none"}} >About</Link></li>
+    //             </ul>
+    //         </div>
+    //             <center> 
+    //                 <h1 style={{"fontFamily": "\'Merriweather\', serif","backgroundColor": "#353b48","padding":"20px", "color":"white"}}>
+    //                     FOODUDE 
+    //                     <img src="https://i.imgur.com/pkMF0BL.png" style={{"width":"9vh"}}></img>
+    //                 </h1>
+    //             </center>
+    //     </React.Fragment>
+    // }
 }
 
 export default Header; 
