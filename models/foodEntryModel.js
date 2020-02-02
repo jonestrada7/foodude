8534e4d49cc61b60132c5644b2d42e34eaca37b6
jonestrada7 <jonathan.estrada747@gmail.com>
2020-02-01 16:28:10 -0800

    Updated new repo with templates

---
+++ models/foodEntryModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+// const slugify = require("slugify");
+
+const foodEntrySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'An entry must have a name']
+        },
+        price: {
+            type: Number,
+            required: [true, 'An entry must have a price']
+        },
+        description: {
+            type: String
+        },
+        locations: [String],
+        image: String,
+        tag1: {
+            type: String,
+            required: [true, 'At least one tag is needed']
+        },
+        tag2: String,
+        tag3: String
+    }
+)
+
+// Enables search by text
+foodEntrySchema.index({ tag1: 'text' });
+
+const foodEntry = mongoose.model('foodEntry', foodEntrySchema);
+
+module.exports = foodEntry;
