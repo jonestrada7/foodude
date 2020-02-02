8a2cbbc0d78538143036750c76ed20de6fcc0527
Terry Nguyen <terryhn1@uci.edu>
2020-02-02 02:48:20 -0800

    Form HTML changed

--- client/src/Form.js
+++ client/src/Form.js
@@ -7,60 +7,46 @@ class Form extends React.Component{
     return <React.Fragment>
        <div className="box"style={{"paddingLeft":"10%","paddingBottom":"2%"}}>
         <form action="/action_page.php">
-            <div style={{"paddingTop": "10px"}} className="container">
-                <div className="row">
-                    <div style={{"paddingRight":"40px","paddingTop": "10px"}}className="span6">
-                         Food:<br></br>
-                        <input id="food"type="text" name="food" required></input>
-                    </div>
-                    <div style={{"paddingRight":"40px", "paddingTop": "10px"}} className="span6">
-                        <div style={{"paddingLeft": "6.5%"}}>Price:<br></br></div>
-                     
-                        <p style={{"paddingLeft": "-20%"}}>$ <input id="price"type="text" name="price" required></input></p>
-                    </div>
-                </div>
-            <br></br>
+            <div className = "form-group">
+              <label for="food">Food</label>
+              <input id="food" className = "form-control" type="text" name="food" placeholder = "Enter Food" required></input>
             </div>
-        <div style={{"paddingRight": "20%"}} className="span12">
-           Description:<br></br>
-            <textarea style={{"padding": "1"}} id="descrip" type="text" name="description"placeholder="Type a brief description"></textarea>
-           
-       </div>
-       <br></br>
-        <div className="row">
-            <div style={{"paddingRight":"1px","paddingTop": "10px","paddingLeft":"2%"}}className="span2">
-                 Tag1:<br></br>
-                <input id="tag1"type="text" name="tag1" required></input>
+            <div className = "form-group">
+              <label for="price">Price</label>
+              <input id="price"type="text" className = "form-control" name="price" placeholder = "Enter Price" required></input>
             </div>
-            <div style={{"paddingRight":"1px","paddingTop": "10px"}}className="span2">
-                Tag2:<br></br>
-               <input id="tag2"type="text" name="tag2"></input>
-           </div>
-           <div style={{"paddingTop": "10px"}}className="span2">
-                Tag3:<br></br>
-                <input id="tag3"type="text" name="tag3"></input>
-            </div>
-        </div>
-        <br></br>
 
-        Location Name:<br></br>
-        {/* <form autocomplete="off" action="/action_page.php">
-            <div className="autocomplete" style={{"paddingRight": "10%","paddingBottom": "2%"}}>
-              <input style={{"width":"100%"}}id="myInput" type="text" name="myCountry" placeholder="Location" required></input>
+            <div className = "form-group">
+              <label for="description">Description</label>
+              <textarea name = "description" className = "form-control" rows = "3" id = "description" placeholder = "Enter Description of your Experience"></textarea>
             </div>
-          </form> */}
-        <div style={{"paddingTop": "10px"}}className="span2">
-          <input id="location" type="text" name="location" required></input>
-        </div>
-          
-        
-        Image:<br></br>
-        <input id="browse" type="file" id="myFile" required></input>
-        <br></br>
-        <br></br>
-        
 
-        <button id="sub" type="button" className = "btn btn-secondary btn-large">Post!!</button>
+            <div className = "form-group">
+              <label for="tag1">Tag1</label>
+              <input id="tag1"type="text" name="tag1" className = "form-control" placeholder = "Enter Tag1" required></input>
+            </div>
+
+            <div className = "form-group">
+              <label for="tag2">Tag2</label>
+              <input id = "tag2" type = "text" name = "tag2" className = "form-control" placeholder = "Enter Tag2"></input>
+            </div>
+
+            <div className = "form-group">
+              <label for ="tag3">Tag3</label>
+              <input id = "tag3" type = "text" name = "tag3" className = "form-control" placeholder = "Enter Tag3"></input>
+            </div>
+
+            <div className = "form-group">
+              <label for = "location">Location</label>
+              <input id="location" type="text" name="location" className = "form-control" placeholder = "Enter Location" required></input>
+            </div>
+
+            <div className = "form-group">
+              <label for = "imageURL">Image URL</label>
+              <input id = "location" type = "text" name = "imageURL" className = "form-control"></input>
+            </div>
+
+            <button id="sub" type="submit" className = "btn btn-secondary btn-large">Post!!</button>
       </form> 
     </div>
     <br></br>
