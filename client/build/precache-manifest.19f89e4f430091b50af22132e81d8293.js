ad2d548835488b022191cc21757d663a9b2ff7d6
jonestrada7 <jonathan.estrada747@gmail.com>
2020-02-03 13:19:44 -0800

    /build now included

---
+++ client/build/precache-manifest.19f89e4f430091b50af22132e81d8293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f884b2dcd047808898826a8b75203159",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5168f286727fa97fffbe",
+    "url": "/static/css/main.46bfee8a.chunk.css"
+  },
+  {
+    "revision": "6ae911d0138f22da5b58",
+    "url": "/static/js/2.e9bf4e8a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.e9bf4e8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5168f286727fa97fffbe",
+    "url": "/static/js/main.5ce13308.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
